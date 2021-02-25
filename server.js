59c936ee0ca6229330397a22fb9893fcfcc24774
JBrickhouse <jbrickhouse777@gmail.com>
2021-02-24 18:00:17 -0600

    Initial boilerplate commit

---
+++ server.js
@@ -0,0 +1,47 @@
+require('dotenv').config()
+
+const path = require('path');
+
+const express = require("express");
+
+const mongoose = require("mongoose");
+const routes = require("./routes");
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Add routes
+app.use(routes);
+
+// Serve up static assets (usually on heroku)
+// Production Mode
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static(path.join(__dirname, 'client/build')));
+	//
+	app.get('*', (req, res) => {
+		res.sendFile(path.join((__dirname = 'client/build/index.html')));
+	});
+}
+// Build Mode
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname + '/client/public/index.html'));
+});
+
+// Connect to the Mongo DB
+mongoose.connect(
+	process.env.MONGODB_URI || "mongodb://localhost/lettuceDB",
+	{
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useCreateIndex: true,
+		useFindAndModify: false
+	}
+);
+
+// Start the API server
+app.listen(PORT, function () {
+	console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+});
