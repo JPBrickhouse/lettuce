fd4d7a4cd49675b960cd431513e882ddafbac7dd
JBrickhouse <jbrickhouse777@gmail.com>
2021-02-25 20:01:06 -0600

    Basic home page framework

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
+
+
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+
+import Home from "./pages/Home/Home"
+
 
 function App() {
 	return (
 		<div>
-			<p>Hello</p>
+			<Home/>
 		</div>
 	);
 }
