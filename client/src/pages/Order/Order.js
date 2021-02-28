1626a44b144d636c2b3f939c520b301e9631a8af
JBrickhouse <jbrickhouse777@gmail.com>
2021-02-27 23:09:13 -0600

    Basic ad copy and page structure

--- client/src/pages/Order/Order.js
+++ client/src/pages/Order/Order.js
@@ -1,5 +1,11 @@
 import React from "react"
 
+// Importing React Bootstrap components
+import Container from "react-bootstrap/Container"
+import Image from "react-bootstrap/Image"
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
+
 const Order = () => {
     return (
         <div>
--- client/src/pages/Rewards/Rewards.js
+++ client/src/pages/Rewards/Rewards.js
@@ -1,9 +1,40 @@
 import React from "react"
 
+// Importing React Bootstrap components
+import Container from "react-bootstrap/Container"
+import Image from "react-bootstrap/Image"
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
+
 const Rewards = () => {
     return (
         <div>
             <p>Rewards</p>
+            <br/>
+            <p>
+                Earn Lettuce Entertain You Frequent Diner points with every ASAP Poke meal!
+            </p>
+            <p>
+                It's easy! Download the app to join the Frequent Diner Club!
+            </p>
+            <p>
+                To earn points, simply add your Frequent DIner number to the comment section of every order.
+            </p>
+            <p>
+                You can earn points at ASAP Poke – and at every Lettuce Entertain You restaurant!
+            </p>
+            <p>
+                Not a member? Download the LettuceEats App ASAP!
+            </p>
+            <p>
+                After you sign-up for the Frequent Diner Club, be sure to sign-up for the ASAP Poke Newsletter, for the inside scoop of ASAP deals!
+            </p>
+            <p>
+                Need a refresher on our menu? Checkout our fresh options!
+            </p>
+            <p>
+                Ready to order? Let's roll!
+            </p>
         </div>
     );
 }
