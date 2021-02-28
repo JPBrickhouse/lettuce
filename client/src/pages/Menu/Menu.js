55f487f5e820bff40af8d376b95ebba4c7e34f0d
JBrickhouse <jbrickhouse777@gmail.com>
2021-02-27 20:15:59 -0600

    Menu styles refined

--- client/src/pages/Menu/Menu.js
+++ client/src/pages/Menu/Menu.js
@@ -29,143 +29,166 @@ const Menu = () => {
             <Container>
                 {/* -------------------------------------------------- */}
                 {/* Headline */}
-                <Row>
-                    <Col>
-                        <h1>Seasoned pro? Let's get you ordering ASAP!</h1>
-                        <br />
-                        <h1>New to ASAP? Welcome! Here's how to build your custom creation!</h1>
-                    </Col>
-                </Row>
+                <div className="smallBotBuffer">
+                    <Row>
+                        <Col>
+                            <h1>Seasoned pro? Let's get you ordering ASAP!</h1>
+                            <br />
+                            <h1>New to ASAP? Welcome! Here's how to build your custom creation!</h1>
+                        </Col>
+                    </Row>
+                </div>
                 {/* -------------------------------------------------- */}
                 {/* Hero Image */}
-                <Row>
-                    <Col>
-                        <Image src={BYO} alt="Custom bowl surrounded by ingredients" fluid />
-                    </Col>
-                </Row>
+                <div className="bottomBuffer">
+                    <Row>
+                        <Col className="justVert">
+                            <Image src={BYO} alt="Custom bowl surrounded by ingredients" fluid />
+                        </Col>
+                    </Row>
+                </div>
                 {/* -------------------------------------------------- */}
                 {/* Step 1 */}
                 {/* Headline */}
-                <Row>
-                    <Col>
-                        <h1>Step 1 - Choose your bowl!</h1>
-                    </Col>
-                </Row>
-                {/* Content */}
-                <Row>
-                    {/* Description */}
-                    <Col>
-                        <Row>
-                            <Col>
-                                <h2>Regular Bowl - 12.95</h2>
-                                <h3>24 oz. Perfect for lunch or dinner!</h3>
-                            </Col>
-                        </Row>
-                        <Row>
-                            <Col>
-                                <h2>Shorty Bowl - 9.95</h2>
-                                <h3>18 oz. Great for a snack or light meal!</h3>
-                            </Col>
-                        </Row>
-                    </Col>
-                    {/* Image */}
-                    <Col>
-                        <Image className="wiggleImg" src={Bowl} alt="Empty bowl awaiting ingredients" fluid />
-                    </Col>
-                </Row>
+                <div className="bottomBuffer">
+                    <Row className="smallBotBuffer">
+                        <Col>
+                            <h1>Step 1 - Choose your bowl!</h1>
+                        </Col>
+                    </Row>
+                    {/* Content */}
+                    <Row className="justVert">
+                        {/* Description */}
+                        <Col md={6}>
+                            <Row>
+                                <Col>
+                                    <h2>Regular Bowl - 12.95</h2>
+                                    <h3>24 oz. Perfect for lunch or dinner!</h3>
+                                </Col>
+                            </Row>
+                            <br/>
+                            <Row>
+                                <Col>
+                                    <h2>Shorty Bowl - 9.95</h2>   
+                                    <h3>18 oz. Great for a snack or light meal!</h3>
+                                </Col>
+                            </Row>
+                        </Col>
+                        {/* Image */}
+                        <Col md={6}>
+                            <Image className="wiggleImg" src={Bowl} alt="Empty bowl awaiting ingredients" fluid />
+                        </Col>
+                    </Row>
+                </div>
                 {/* -------------------------------------------------- */}
                 {/* Step 2 */}
                 {/* Headline */}
-                <Row>
-                    <Col>
-                        <h1>Step 2 - Choose your base!</h1>
-                    </Col>
-                </Row>
-                {/* Content */}
-                <Row>
-                    {/* Image */}
-                    <Col>
-                        <Image className="wiggleImg" src={ChooseBase} alt="Base options of rice, quiona, and greens" fluid />
-                    </Col>
-                    {/* Description */}
-                    <Col>
-                        <h2>Brown Rice</h2>
-                        <h2>Greens</h2>
-                        <h2>Quinoa Tabouli</h2>
-                        <h2>Sushi Rice</h2>
-                    </Col>
-                </Row>
+                <div className="bottomBuffer">
+                    <Row className="smallBotBuffer">
+                        <Col>
+                            <h1>Step 2 - Choose your base!</h1>
+                        </Col>
+                    </Row>
+                    {/* Content */}
+                    <Row className="justVert">
+                        {/* Image */}
+                        <Col md={6}>
+                            <Image className="wiggleImg" src={ChooseBase} alt="Base options of rice, quiona, and greens" fluid />
+                        </Col>
+                        {/* Description */}
+                        <Col md={6}>
+                            <h1>Brown Rice</h1>
+                            <br/>
+                            <h1>Greens</h1>
+                            <br/>
+                            <h1>Quinoa Tabouli</h1>
+                            <br/>
+                            <h1>Sushi Rice</h1>
+                        </Col>
+                    </Row>
+                </div>
                 {/* -------------------------------------------------- */}
                 {/* Step 3 */}
                 {/* Headline */}
-                <Row>
-                    <Col>
-                        <h1>Step 3 - Choose your poke!</h1>
-                    </Col>
-                </Row>
-                {/* Content - Images and Description */}
-                <Row>
-                    <Col>
-                        <h2>Ahi Tuna</h2>
-                        <Image className="wiggleImg" src={Tuna} alt="Ahi Tuna" fluid />
-                    </Col>
-                    <Col>
-                        <h2>Atlantic Salmon</h2>
-                        <Image className="wiggleImg" src={Salmon} alt="Salmon" fluid />
-                    </Col>
-                    <Col>
-                        <h2>Gingered Beets</h2>
-                        <Image className="wiggleImg" src={Beets} alt="Gingered Beets" fluid />
-                    </Col>
-                </Row>
+                <div className="bottomBuffer">
+                    <Row className="smallBotBuffer">
+                        <Col>
+                            <h1>Step 3 - Choose your poke!</h1>
+                        </Col>
+                    </Row>
+                    {/* Content - Images and Description */}
+                    <Row>
+                        <Col md={4}>
+                            <h2>Ahi Tuna</h2>
+                            <Image className="wiggleImg" src={Tuna} alt="Ahi Tuna" fluid />
+                        </Col>
+                        <Col md={4}>
+                            <h2>Atlantic Salmon</h2>
+                            <Image className="wiggleImg" src={Salmon} alt="Salmon" fluid />
+                        </Col>
+                        <Col md={4}>
+                            <h2>Gingered Beets</h2>
+                            <Image className="wiggleImg" src={Beets} alt="Gingered Beets" fluid />
+                        </Col>
+                    </Row>
+                </div>
                 {/* -------------------------------------------------- */}
                 {/* Step 4 */}
-                <Row>
-                    <Col>
-                        <h1>Step 4 - Choose your style!</h1>
-                    </Col>
-                </Row>
-                <Row>
-                    {/* Classic Avocado */}
-                    <Col>
-                        <h3>{styleOptions[0].name}</h3>
-                        <ul>
-                            {styleOptions[0].ingredients.map((item) => <li key={item}>{item}</li>)}
-                        </ul>
-                    </Col>
-                    {/* Big Island Spicy */}
-                    <Col>
-                        <h3>{styleOptions[1].name}</h3>
-                        <ul>
-                            {styleOptions[1].ingredients.map((item) => <li key={item}>{item}</li>)}
-                        </ul>
-                    </Col>
-                    {/* Maui Crunch */}
-                    <Col>
-                        <h3>{styleOptions[2].name}</h3>
-                        <ul>
-                            {styleOptions[2].ingredients.map((item) => <li key={item}>{item}</li>)}
-                        </ul>
-                    </Col>
-                    {/* Ginger-Yuzu */}
-                    <Col>
-                        <h3>{styleOptions[3].name}</h3>
-                        <ul>
-                            {styleOptions[3].ingredients.map((item) => <li key={item}>{item}</li>)}
-                        </ul>
-                    </Col>
-                </Row>
+                <div className="bottomBuffer">
+                    <Row className="smallBotBuffer">
+                        <Col>
+                            <h1>Step 4 - Choose your style!</h1>
+                        </Col>
+                    </Row>
+                    <Row>
+                        {/* Classic Avocado */}
+                        <Col md={3}>
+                            <h3>{styleOptions[0].name}</h3>
+                            <ul>
+                                {styleOptions[0].ingredients.map((item) => <li key={item}>{item}</li>)}
+                            </ul>
+                        </Col>
+                        {/* Big Island Spicy */}
+                        <Col md={3}>
+                            <h3>{styleOptions[1].name}</h3>
+                            <ul>
+                                {styleOptions[1].ingredients.map((item) => <li key={item}>{item}</li>)}
+                            </ul>
+                        </Col>
+                        {/* Maui Crunch */}
+                        <Col md={3}>
+                            <h3>{styleOptions[2].name}</h3>
+                            <ul>
+                                {styleOptions[2].ingredients.map((item) => <li key={item}>{item}</li>)}
+                            </ul>
+                        </Col>
+                        {/* Ginger-Yuzu */}
+                        <Col md={3}>
+                            <h3>{styleOptions[3].name}</h3>
+                            <ul>
+                                {styleOptions[3].ingredients.map((item) => <li key={item}>{item}</li>)}
+                            </ul>
+                        </Col>
+                    </Row>
+                </div>
                 {/* -------------------------------------------------- */}
                 {/* FINAL Image - Let's order! */}
-                <Row>
-                    <Col>
-                        <h1>That's it! You've built your very own ASAP Poke Bowl</h1>
-                        <br />
-                        <Image src={FinalBowl} alt="Final Poke bowl" fluid />
-                        <br />
-                        <h1>Ready to order? Let's roll!</h1>
-                    </Col>
-                </Row>
+                <div className="smallBotBuffer">
+                    <Row>
+                        <Col>
+                            <h1>That's it! You've built your very own ASAP Poke Bowl</h1>
+                            <br />
+                            <Image src={FinalBowl} alt="Final Poke bowl" fluid />
+                        </Col>
+                    </Row>
+                </div>
+                <div>
+                    <Row>
+                        <Col>
+                            <h1>Ready to order? Let's roll!</h1>
+                        </Col>
+                    </Row>
+                </div>
             </Container>
         </div>
     );
